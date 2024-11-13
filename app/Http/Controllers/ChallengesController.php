e990b608b01e642fcd03c211db89eb4ec288fba6
egido-quan <isidro.zapater@gmail.com>
2024-11-14 00:13:48 +0100

    Mejora en la lógica del resultado con tie break

--- app/Http/Controllers/ChallengesController.php
+++ app/Http/Controllers/ChallengesController.php
@@ -13,7 +13,7 @@ class ChallengesController extends Controller
     public function show($player_id) {
         $players = Player::orderBy('ranking')->get();
         $player = Player::where('id', $player_id)
-        ->with('challenges_1') //Tengo que mirar si el jugador está en el campo player_1 o player_2
+        ->with('challenges_1') //Hay que comprobar si el jugador está en el campo player_1 o player_2
         ->with('challenges_2')
         ->first();
 
@@ -31,7 +31,7 @@ class ChallengesController extends Controller
                     $ch1[$i]["p2_set1"], $ch1[$i]["p2_set2"], $ch1[$i]["p2_set3"],
                     $ch1[$i]["created_at"]->format('d-m-Y')
                     ];
-                } else { //Tengo que saber si el jugador es player_1 o player_2 para poner bien el orden en el resultado
+                } else { //Hay que saber si el jugador es player_1 o player_2 para poner bien el orden en el resultado
                     $challenges[] =
                     [Player::where('id', $other_player_id)->value('name'),
                     Player::where('id', $other_player_id)->value('family_name'),
@@ -87,18 +87,43 @@ class ChallengesController extends Controller
     public function save(Request $request) {
 
         $players = Player::orderBy('ranking')->get();
-        $message = match(true) {
-            $request->player1_ranking === $request->player2_ranking => 'One player cannot challenge himself',
-            abs($request->p1_set1 - $request->p2_set1) < 2 => 'First set result is not correct',
-            abs($request->p1_set2 - $request->p2_set2) < 2 => 'Second set result is not correct',
-            abs($request->player1_ranking - $request->player2_ranking) > 3 
-                => 'Cannot accept this challenge, sorry. A challenge is only allowed when ranking difference is 3 positions or lower',
-            default => "OK",
-        };
-
-        if ($message === "OK" && ($request->p1_set3 != null || $request->p2_set3 != null)) {  
-            $message = (abs($request->p1_set3 - $request->p2_set3) < 2) ? 'Third set result is not correct' : 'OK';
+        if ($request->player1_ranking === $request->player2_ranking) {
+            $message = 'One player cannot challenge himself';
+        } elseif (abs($request->player1_ranking - $request->player2_ranking) > 3) {
+            $message = 'Cannot accept this challenge, sorry. A challenge is only allowed when ranking difference is 3 positions or lower';
+        } elseif (($request->p1_set1 == 7 || $request->p2_set1 == 7) && abs($request->p1_set1 - $request->p2_set1) > 2) {
+            $message = 'First set result is not correct';
+        } elseif (($request->p1_set2 == 7 || $request->p2_set2 == 7) && abs($request->p1_set2 - $request->p2_set2) > 2) {   
+            $message = 'Second set result is not correct';
+        } elseif (($request->p1_set1 < 7 && $request->p2_set1 < 7) && abs($request->p1_set1 - $request->p2_set1) < 2) {
+            $message = 'First set result is not correct';
+        } elseif (($request->p1_set2 < 7 && $request->p2_set2 < 7) && abs($request->p1_set2 - $request->p2_set2) < 2) {
+            $message = 'Second set result is not correct';
+        } elseif ($request->p1_set1 < 6 && $request->p2_set1 < 6) {
+            $message = 'First set result is not correct';
+        } elseif ($request->p1_set2 < 6 && $request->p2_set2 < 6) {
+            $message = 'Second set result is not correct';
+        } else {
+            $message = 'OK';
         }
+        
+        if (!($message === "OK")) {
+            return view('challenges.addError', [
+                'players' => $players,
+                'message' => $message
+            ]);
+        }
+
+        if (($request->p1_set3 != null || $request->p2_set3 != null)) {
+            if (($request->p1_set3 == 7 || $request->p2_set3 == 7) && abs($request->p1_set3 - $request->p2_set3) > 2) {
+                $message = 'Third set result is not correct';
+            } elseif (($request->p1_set3 < 7 && $request->p2_set3 < 7) && abs($request->p1_set3 - $request->p2_set3) < 2) {
+                $message = 'Third set result is not correct';
+            } elseif ($request->p1_set3 < 6 && $request->p2_set3 < 6) {
+                $message = 'Third set result is not correct';
+            }
+        }
+        
 
         if (!($message === "OK")) {
             return view('challenges.addError', [
@@ -108,14 +133,12 @@ class ChallengesController extends Controller
         }
 
         if ($request->p1_set3 == null && $request->p2_set3 == null) { 
-            $p1_set3 = 0;
-            $p2_set3 = 0;
+            $score = [$request->p1_set1, $request->p2_set1, $request->p1_set2, $request->p2_set2];
         } else {
-            $p1_set3 = $request->p1_set3;
-            $p2_set3 = $request->p2_set3;
+            $score = [$request->p1_set1, $request->p2_set1, $request->p1_set2, $request->p2_set2, $request->p1_set3, $request->p1_set3];
         }
 
-        $score = [$request->p1_set1, $request->p2_set1, $request->p1_set2, $request->p2_set2, $p1_set3, $p2_set3];
+        
         $marcador = new Marcador($score, $request->player1_ranking, $request->player2_ranking);
         $player1 = Player::where('ranking', $request->player1_ranking)->first();
         $player2 = Player::where('ranking', $request->player2_ranking)->first();
